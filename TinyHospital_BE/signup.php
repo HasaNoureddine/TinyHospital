0916b29b2b0626e3dedd647dc6635649541ddd65
HasaNoureddine <hasannoureddine351@gmail.com>
2023-03-11 20:04:50 +0200

    initializing files

---
+++ TinyHospital_BE/signup.php
@@ -0,0 +1,15 @@
+<?php
+
+include('connection.php');
+
+$name=$_POST["name"];
+$dob=$_POST["dob"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+$position=$_POST["position"];
+
+
+
+
+
+?>
