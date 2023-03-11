921d12a4c71e4f687e18cf4b04c5a674df83b400
HasaNoureddine <hasannoureddine351@gmail.com>
2023-03-11 20:11:17 +0200

    fetching usertypes

--- TinyHospital_BE/signup.php
+++ TinyHospital_BE/signup.php
@@ -16,6 +16,16 @@ $query -> bind_param("ss", $password, $email);
 $query -> execute();
 $result = $query -> get_result();
 
+$query_usertype = $mysql -> prepare("SELECT `iduser_types` FROM `user_types` where  `name`=?  ");
+$query_usertype -> bind_param("s", $position);
+$query_usertype  -> execute();
+$id = $query_usertype -> get_result();
+$usertype_id = $id->fetch_assoc()["iduser_types"];
+
+
+while($row = $result->fetch_assoc()) {
+    $data[]=$row;
+}
 
 
 
