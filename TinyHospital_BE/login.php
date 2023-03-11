00c7ec3e19e139837046b8a8df96ee0349d13794
HasaNoureddine <hasannoureddine351@gmail.com>
2023-03-11 20:22:14 +0200

    verifying password for users

--- TinyHospital_BE/login.php
+++ TinyHospital_BE/login.php
@@ -20,6 +20,32 @@
             "user_type" => "admin"
         ];  
 
+    }else{
+        $query = $mysql -> prepare("SELECT * FROM `users` WHERE `email`=? ");
+        $query -> bind_param("s", $email);
+        $query -> execute();
+        $result = $query -> get_result();
+        $object = $result -> fetch_assoc();
+    
+        if($object["usertype_id"]==2 || $object["usertype_id"]==3 && password_verify($password,$object["password"]) ){
+                $response = [
+                    "idusers" => $object["idusers"],
+                    "name" => $object["name"],
+                    "email" => $object["email"],
+                    "password" => $object["password"],
+                    "dob" => $object["dob"],
+                    "user_type" => "user",
+                ];
+                
+        }else{
+            $response =[
+                "message" => "Credentials are incorrect"
+            ];
+        };
     }
+
+
+
+echo json_encode($response)
 
 ?>
