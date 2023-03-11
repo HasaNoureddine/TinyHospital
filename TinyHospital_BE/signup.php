20295d6a5bbede52d14864e51840270db1007b53
HasaNoureddine <hasannoureddine351@gmail.com>
2023-03-11 20:09:56 +0200

    adding query in signup

--- TinyHospital_BE/signup.php
+++ TinyHospital_BE/signup.php
@@ -9,6 +9,15 @@ $email = $_POST["email"];
 $position=$_POST["position"];
 
 
+$hashed = password_hash($password,PASSWORD_BCRYPT);
+
+$query = $mysql -> prepare("SELECT * FROM `users` where  `password`=? and email=? ");
+$query -> bind_param("ss", $password, $email);
+$query -> execute();
+$result = $query -> get_result();
+
+
+
 
 
 
