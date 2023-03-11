af2d057a5657ae16275af18521a9aa7fc8258850
HasaNoureddine <hasannoureddine351@gmail.com>
2023-03-11 20:20:07 +0200

    using connection_obj to fetch password

--- TinyHospital_BE/login.php
+++ TinyHospital_BE/login.php
@@ -7,6 +7,12 @@
 
     
 
+    $query = $mysql -> prepare("SELECT `password` FROM `users` where  email=? ");
+    $query -> bind_param("s",$email );
+    $query -> execute();
+    $result = $query -> get_result();
+    $object = $result -> fetch_assoc();
+  
 
 
 
