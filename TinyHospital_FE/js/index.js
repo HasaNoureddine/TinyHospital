12ca20858e52b32a7b41fd7cc8f2de4bbba5d718
HasaNoureddine <hasannoureddine351@gmail.com>
2023-03-12 21:39:46 +0200

    adding get hospitals

--- TinyHospital_FE/js/index.js
+++ TinyHospital_FE/js/index.js
@@ -17,7 +17,7 @@ axios({
           <p>${user.name}</p>
           <p>${user.dob}</p>
       </div>
-      <button id="assign"><b>Assign</b></button>
+      <button id="assign" value="${user.idusers}"><b>Assign</b></button>
   </div>
     `;
     }
@@ -27,9 +27,34 @@ axios({
 let assign_btn = document.getElementById("assign");
 submit_btn.onclick = (e) => {
   e.preventDefault();
-  add_product();
+  assign_patient();
 };
-function add_product() {
+function assign_patient() {
+  axios({
+    method: "post",
+    url: `http://localhost/TinyHospital/TinyHospital_BE/get_users.php`,
+  }).then(async (res) => {
+    const users = res.data.users;
+    console.log(users);
+    users.forEach((user) => {
+      console.log(`loop`);
+      if (user.usertype_id == 3) {
+        console.log(`condition`);
+        UserContainer.innerHTML += `
+      <div class="card">
+      <img src="../TinyHospital_FE/images/patient (2).svg" alt="Avatar" style="width:100%">
+      <div class="container">
+          <h4><b>${user.idusers}</b></h4>
+          <p>${user.name}</p>
+          <p>${user.dob}</p>
+      </div>
+      <button id="assign" value="${user.idusers}"><b>Assign</b></button>
+  </div>
+    `;
+      }
+    });
+  });
+
   let name = document.getElementById("fname").value;
   let brand = document.getElementById("brand").value;
   let price = document.getElementById("price").value;
